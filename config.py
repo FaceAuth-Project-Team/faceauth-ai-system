eb34d5c7b44db44e8024f11de2e5bc2111ca5d02
Yordanos Teshome <yyordi2003@gmail.com>
2026-03-28 09:57:33 +0300

    commit configuration & requirements

---
+++ config.py
@@ -0,0 +1,7 @@
+# --- Image Settings ---
+# Everyone MUST use these dimensions so the model doesn't crash
+IMG_SIZE = (64, 64) 
+GRAYSCALE = True
+
+# --- ML Settings ---
+CONFIDENCE_THRESHOLD = 0.8  # Minimum match score to 'Grant Access'
