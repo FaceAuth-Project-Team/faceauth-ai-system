3f5198437df42754e6ede034156f41290f2906fa
Yordanos Teshome <yyordi2003@gmail.com>
2026-04-05 22:32:57 +0300

    Delete src directory

--- src/image_processing.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-def preprocess_image(img):
-    # Ensure image is resized to match training size
-    img = cv2.resize(img, (64, 64))
-    # Normalize pixel values to be between 0 and 1
-    img = img.flatten() / 255.0
-    return img
